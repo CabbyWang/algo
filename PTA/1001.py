e9be3c994390d1a530d9ed7cbabffa7735d721d2
wangsiyong <18489969713@163.com>
2018-05-22 10:18:57 +0800

    PAT 1001

---
+++ PTA/1001.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# coding:utf-8
+
+def old_iter():
+    """正整数"""
+    n = 0
+    while True:
+        n += 1
+        yield n
+
+
+if __name__ == '__main__':
+    n = input()
+    step = 0
+    while n != 1:
+        step += 1
+        if n % 2 == 0:
+            n = n / 2
+        else:
+            n = (3 * n + 1) / 2
+    print(step)
